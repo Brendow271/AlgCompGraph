d325a5ca9c18e47036640a021ad956090b836789
Brendow271 <pashashkuropat@yandex.ru>
2025-04-13 02:18:57 +0300

    Update Shaders for Instansing

--- AlgCompGraph/AlgCompGraph/PixelShader.hlsl
+++ AlgCompGraph/AlgCompGraph/PixelShader.hlsl
@@ -1,4 +1,5 @@
 Texture2D diffuseMap : register(t0);
+Texture2D diffuseMap_2 : register(t2);
 Texture2D normalMap : register(t1);
 SamplerState samLinear : register(s0);
 
@@ -20,31 +21,42 @@ struct PS_INPUT
     float3 WorldPos : TEXCOORD0;
     float3 Normal : TEXCOORD1;
     float2 TexCoord : TEXCOORD2;
+    nointerpolation uint texIndex : TEXCOORD3;
 };
 
 float3 ComputeTangent(float3 n)
 {
-    return (abs(n.y) > 0.99) ? float3(1, 0, 0) : normalize(cross(float3(0, 1, 0), n));
+    float3 tangent = float3(1, 0, 0);
+    if (dot(n, tangent) > 0.99)
+        tangent = float3(0, 0, 1);
+    return normalize(tangent - n * dot(n, tangent));
 }
 
 float4 PSMain(PS_INPUT input) : SV_Target
 {
-    float4 diffuseColor = diffuseMap.Sample(samLinear, input.TexCoord);
-    float3 normalSample = normalMap.Sample(samLinear, input.TexCoord).rgb * 2.0 - 1.0;
-
-    float3 tangent = ComputeTangent(normalize(input.Normal));
-    float3 bitangent = normalize(cross(normalize(input.Normal), tangent));
-    float3x3 TBN = float3x3(tangent, bitangent, normalize(input.Normal));
-
-    float3 perturbedNormal = normalize(mul(normalSample, TBN));
     
+    float4 diffuseColor;
+    if (input.texIndex == 1)
+    {
+        diffuseColor = diffuseMap.Sample(samLinear, input.TexCoord);
+        float3 normalSample = normalMap.Sample(samLinear, input.TexCoord).rgb * 2.0 - 1.0;
+        float3 tangent = ComputeTangent(normalize(input.Normal));
+        float3 bitangent = normalize(cross(normalize(input.Normal), tangent));
+        float3x3 TBN = float3x3(tangent, bitangent, normalize(input.Normal));
+        input.Normal = normalize(mul(normalSample, TBN));
+    }
+    else
+    {
+        diffuseColor = diffuseMap_2.Sample(samLinear, input.TexCoord);
+    }
+
     float3 lightDir = normalize(lightPos - input.WorldPos);
     float lightDirLength = length(lightPos - input.WorldPos);
     float attenuation = clamp(1 / (lightDirLength * lightDirLength), 0, 1);
-    
-    float diff = saturate(dot(perturbedNormal, lightDir));
+
+    float diff = saturate(dot(normalize(input.Normal), lightDir));
     float3 viewDir = normalize(cameraPosition - input.WorldPos);
-    float3 reflectDir = reflect(-lightDir, perturbedNormal);
+    float3 reflectDir = reflect(-lightDir, normalize(input.Normal));
     float spec = pow(saturate(dot(viewDir, reflectDir)), 32);
 
     float3 ambientColor = ambient * diffuseColor.rgb;
--- AlgCompGraph/AlgCompGraph/VertexShader.hlsl
+++ AlgCompGraph/AlgCompGraph/VertexShader.hlsl
@@ -1,7 +1,11 @@
+#define NUM_INSTANCES 10
+#define NUM_TEX 2
+
 cbuffer ModelBuffer : register(b0)
 {
-    float4x4 model;
-    float4x4 normal;
+    float4x4 models[NUM_INSTANCES];
+    float4x4 normals[NUM_INSTANCES];
+    float4 isNormalMapActive[NUM_INSTANCES];
 };
 
 cbuffer VPBuffer : register(b1)
@@ -14,6 +18,7 @@ struct VSInput
     float3 position : POSITION;
     float3 normal : NORMAL;
     float2 texcoord : TEXCOORD;
+    uint instanceID : SV_InstanceID;
 };
 
 struct VSOutput
@@ -22,15 +27,18 @@ struct VSOutput
     float3 worldPos : TEXCOORD0;
     float3 normal : TEXCOORD1;
     float2 texcoord : TEXCOORD2;
+    nointerpolation uint texIndex : TEXCOORD3;
 };
 
 VSOutput VSMain(VSInput input)
 {
     VSOutput output;
-    float4 worldPos = mul(model,float4(input.position, 1.0));
+    uint instanceID = input.instanceID;
+    float4 worldPos = mul(models[instanceID], float4(input.position, 1.0));
     output.position = mul(vp, worldPos);
     output.worldPos = worldPos.xyz;
-    output.normal = mul((float3x3) normal, input.normal);
+    output.normal = mul((float3x3) normals[instanceID], input.normal);
     output.texcoord = input.texcoord;
+    output.texIndex = isNormalMapActive[instanceID].x > 0 ? 1 : 0;
     return output;
 }
